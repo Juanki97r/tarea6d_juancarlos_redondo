2f4bce691da1766fc080ceb369f7cf7e41fa58a3
NickGconroc <NickGconroc@users.noreply.github.com>
2026-04-21 14:11:55 +0200

    punto4

--- src/main/java/daw/StreamJava.java
+++ src/main/java/daw/StreamJava.java
@@ -1,13 +1,26 @@
 package daw;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class StreamJava {
-    
+    //1
     public static void primerosCinco(List<Vehiculo> lista) {
         lista.stream()
                 .limit(5)
                 .forEach(v-> System.out.println(v));;
+    }
+    //2
+     public static void vehiculos2012(List<Vehiculo> lista) {
+        lista.stream()
+                .filter(v -> v.getVehicle_year() == 2012)
+                .forEach(System.out::println);
+    }
+    //3
+    public static Set<String> marcasDistintas(List<Vehiculo> lista) {
+        return lista.stream()
+                .map(Vehiculo::getVehicle_make)
+                .collect(Collectors.toSet());
     }
 }
