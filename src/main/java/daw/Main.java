f2dd3154d862875a1f08a34a80237a6072fd7144
Juanki97r <juanki97r@gmail.com>
2026-04-21 17:45:37 +0200

    acabado

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -1,16 +1,75 @@
 package daw;
 
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalInt;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         
         List<Vehiculo> vehiculos = LeerCsv.leerVehiculosDesdeCSV("vehiculos.csv");
        
-        //1) 
+        //1)
+        System.out.println("----------1---------------");
         StreamJava.primerosCinco(vehiculos);
-        
+        //2
+         System.out.println("----------2---------------");
+        StreamJava.vehiculos2012(vehiculos);
+        //3
+         System.out.println("----------3---------------");
+        Set<String> marcasDistintas = StreamJava.marcasDistintas(vehiculos);
+        for (String m : marcasDistintas) {
+            System.out.println(m);
+        }
+        //4
+        System.out.println("----------4---------------");
+        List<Vehiculo> ordenadoPorAnio = StreamJava.ordenarPorAnio(vehiculos);
+        for (Vehiculo v : ordenadoPorAnio) {
+            System.out.println(v);
+        }
+        //5
+        System.out.println("----------5---------------");
+        List<Vehiculo> saltar10 = StreamJava.saltar10Limit5(ordenadoPorAnio);
+        for (Vehiculo vehiculo : saltar10) {
+            System.out.println(vehiculo);
+        }
+        //6
+        System.out.println("----------6---------------");
+        boolean verde = StreamJava.existeColorVerde(vehiculos);
+        System.out.println(verde);
+        //7
+        System.out.println("----------7---------------");
+        boolean todos1990 = StreamJava.todosDesde1990(vehiculos);
+        System.out.println(todos1990);
+        //8
+        System.out.println("----------8---------------");
+        long cantidadFord = StreamJava.contarFord(vehiculos);
+        System.out.println(cantidadFord);
+        //9
+        System.out.println("----------9---------------");
+        OptionalInt menorAnio = StreamJava.anioMasPequenio(vehiculos);
+        System.out.println(menorAnio);
+        //10
+        System.out.println("----------10---------------");
+        Set<String> coloresPost2010 = StreamJava.coloresPost2010(vehiculos);
+        for (String color : coloresPost2010) {
+            System.out.println(color);
+        }
+        //11
+        System.out.println("----------11---------------");
+        Map<String,Long> contarPorColor = StreamJava.contarPorColor(vehiculos);
+        contarPorColor.forEach((key,value)->{
+            System.out.println("Color: " + key + " , Cantidad: "+ value);
+        });
 
+        //12
+        System.out.println("----------12---------------");
+        Map<String,List<Vehiculo>> porMatricula = StreamJava.agruparPorMatricula(vehiculos);
+        porMatricula.forEach((key,value)->{
+            System.out.println("Matrícula: \n "+key+", Vehiculo: "+value );
+            
+        });
     }
 
     
--- src/main/java/daw/StreamJava.java
+++ src/main/java/daw/StreamJava.java
@@ -1,6 +1,9 @@
 package daw;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.OptionalInt;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -22,5 +25,70 @@ public class StreamJava {
         return lista.stream()
                 .map(Vehiculo::getVehicle_make)
                 .collect(Collectors.toSet());
+    }
+    // 4
+    public static List<Vehiculo> ordenarPorAnio(List<Vehiculo> lista) {
+        return lista.stream()
+                .sorted(Comparator.comparingInt(Vehiculo::getVehicle_year))
+                .collect(Collectors.toList());
+    }
+
+    // 5
+    public static List<Vehiculo> saltar10Limit5(List<Vehiculo> lista) {
+        return lista.stream()
+                .skip(10)
+                .limit(5)
+                .collect(Collectors.toList());
+    }
+
+    // 6
+    public static boolean existeColorVerde(List<Vehiculo> lista) {
+        return lista.stream()
+                .anyMatch(v -> "Green".equalsIgnoreCase(v.getVehicle_color()));
+    }
+
+    // 7
+    public static boolean todosDesde1990(List<Vehiculo> lista) {
+        return lista.stream()
+                .allMatch(v -> v.getVehicle_year() >= 1990);
+    }
+
+    // 8
+    public static long contarFord(List<Vehiculo> lista) {
+        return lista.stream()
+                .filter(v -> "Ford".equalsIgnoreCase(v.getVehicle_make()))
+                .count();
+    }
+
+    // 9
+    public static OptionalInt anioMasPequenio(List<Vehiculo> lista) {
+        return lista.stream()
+                .mapToInt(Vehiculo::getVehicle_year)
+                .min();
+    }
+
+    // 10
+    public static Set<String> coloresPost2010(List<Vehiculo> lista) {
+        return lista.stream()
+                .filter(v -> v.getVehicle_year() > 2010)
+                .map(Vehiculo::getVehicle_color)
+                .collect(Collectors.toSet());
+    }
+
+    // 11
+    public static Map<String, Long> contarPorColor(List<Vehiculo> lista) {
+        return lista.stream()
+                .collect(Collectors.groupingBy(
+                        Vehiculo::getVehicle_color,
+                        Collectors.counting()
+                ));
+    }
+
+    // 12
+    public static Map<String, List<Vehiculo>> agruparPorMatricula(List<Vehiculo> lista) {
+        return lista.stream()
+                .collect(Collectors.groupingBy(
+                        Vehiculo::getVehicle_license_plate
+                ));
     }
 }
